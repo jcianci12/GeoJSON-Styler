5d9c24ba43632eacdde083decc23da39f47dc7b2
CianciJ <CianciJ@QH11329889>
2023-03-17 09:24:09 +1000

    refactored handle polygon

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,7 +7,7 @@ import { Bounds, FeatureGroup, geoJSON, latLng, Layer, LayerGroup, Map, MapOptio
 import 'leaflet.fullscreen';
 import { CSVtoJSONPipe } from '../csvtojsonpipe';
 
-import { colour, opacity, text } from '../data/data.component';
+import { colour, opacity, stylerule, text } from '../data/data.component';
 import { FeatureCollectionLayer } from '../featureCollection';
 import { FeaturecollectionService } from '../featurecollection.service';
 import { FeaturefilterPipe } from '../featurefilter.pipe';
@@ -51,7 +51,7 @@ export class MapComponent implements OnInit {
   public map: Map | undefined;
   public zoom: number | undefined;
 
-  constructor(private fcs: FeaturecollectionService,private snackbar:MatSnackBar) {}
+  constructor(private fcs: FeaturecollectionService, private snackbar: MatSnackBar) {}
 
   ngOnInit() {
     this.sub = this.fcs.FeatureCollectionLayerObservable.subscribe((f) => {
@@ -91,8 +91,8 @@ export class MapComponent implements OnInit {
         ?.filter((f) => f.active)
         .forEach((f, i) => {
           let ffp = new FeaturefilterPipe().transform(f, this._featureCollection[i].terms?.triggerval);
-          let styledata = new CSVtoJSONPipe().csvJSON( this._featureCollection[i].styledata as any);
-              let stylerules = this._featureCollection[i].stylerules;
+          let styledata = new CSVtoJSONPipe().csvJSON(this._featureCollection[i].styledata as any);
+          let stylerules = this._featureCollection[i].stylerules;
           ffp.features.forEach((feature) => {
             //add the layer to the group
             let _fc = this._featureCollection[i];
@@ -102,70 +102,24 @@ export class MapComponent implements OnInit {
             let csvgeocolumn = _fc.geocolumn?.GEOColumn.toString();
 
             if (geocolumn) {
-
               //get the index of the geo column
-              if(styledata.length){
-                let geocolumnindex = styledata[0]?.findIndex(col=>col.toLowerCase()== csvgeocolumn.toLowerCase());
-              let propertytomatch = feature.properties?.[geocolumn];
-              styledata.forEach((stylerow) => {
-                //use the geocolumn index
-                let _suburb = stylerow[geocolumnindex];
+              if (styledata.length) {
+                let geocolumnindex = styledata[0]?.findIndex((col) => col.toLowerCase() == csvgeocolumn.toLowerCase());
+                let propertytomatch = feature.properties?.[geocolumn];
+                styledata.forEach((stylerow) => {
+                  //use the geocolumn index
+                  let _suburb = stylerow[geocolumnindex];
 
-                if (propertytomatch && _suburb && propertytomatch.toLowerCase() == _suburb.toLowerCase()) {
-                  let geo = geoJSON(feature);
-                  stylerules.forEach((s) => {
-                    let styledatacolumnindex = styledata[0].indexOf(s.column);
-                    let value = stylerow[styledatacolumnindex];
+                  if (propertytomatch && _suburb && propertytomatch.toLowerCase() == _suburb.toLowerCase()) {
+                    stylerules.forEach((s) => {
+                      var geo = this.handlePolygon(s, feature, stylerow, i, _fc);
 
-                    switch (s.ruletype.rulename) {
-                      case 'opacity': {
-                        geo.setStyle({
-                          fillOpacity: Number.parseFloat(value),
-                        });
-                        break;
-                      }
-                      case 'colour': {
-                        let a = s.ruletype as colour;
-
-                        //if the style has been set globally, use that value
-                        if (a.colour) {
-                          value = a.colour;
-                        }
-
-                        geo.setStyle({
-                          fillColor: value,
-                          color: value,
-                        });
-                        break;
-                      }
-                      case 'text': {
-                        if (true) {
-                          let lat = (s.ruletype as text).latoffset + geo.getBounds().getCenter().lat;
-                          let lng = (s.ruletype as text).lngoffset + geo.getBounds().getCenter().lng;
-
-                          let label = L.marker([lat, lng], {
-                            icon: L.divIcon({
-                              className: 'text-labels', // Set class for CSS styling
-                              html: '<div style="' + (s.ruletype as text).cssstyle + '">' + value + '</div>',
-                            }),
-                            zIndexOffset: 1000, // Make appear above other map features
-                          });
-                          //apply the offset
-
-                          label.addTo(this.featureGroup);
-                        }
-                        break;
-                      }
-                    }
-
-                    let l = geo.addTo(this.featureGroup);
-                    this.featureGroup.addLayer(l);
-                  });
-                }
-              });
+                      let l = geo.addTo(this.featureGroup);
+                      this.featureGroup.addLayer(l);
+                    });
+                  }
+                });
               }
-
-
             }
           });
           this.featureGroup.addTo(this.map!);
@@ -176,5 +130,53 @@ export class MapComponent implements OnInit {
           }
         });
     }
+  }
+  handlePolygon(s: stylerule, feature: geojson.Feature<geojson.Geometry, geojson.GeoJsonProperties>, stylerow: string[], i: number, _fc: FeatureCollectionLayer): L.GeoJSON<any> {
+    let geo = geoJSON(feature);
+    let styledata = new CSVtoJSONPipe().csvJSON(this._featureCollection[i].styledata as any);
+    let styledatacolumnindex = styledata[0].indexOf(s.column);
+    let value = stylerow[styledatacolumnindex];
+
+    switch (s.ruletype.rulename) {
+      case 'opacity': {
+        geo.setStyle({
+          fillOpacity: Number.parseFloat(value),
+        });
+        break;
+      }
+      case 'colour': {
+        let a = s.ruletype as colour;
+
+        //if the style has been set globally, use that value
+        if (a.colour) {
+          value = a.colour;
+        }
+
+        geo.setStyle({
+          fillColor: value,
+          color: value,
+        });
+        break;
+      }
+      case 'text': {
+        if (true) {
+          let lat = (s.ruletype as text).latoffset + geo.getBounds().getCenter().lat;
+          let lng = (s.ruletype as text).lngoffset + geo.getBounds().getCenter().lng;
+
+          let label = L.marker([lat, lng], {
+            icon: L.divIcon({
+              className: 'text-labels', // Set class for CSS styling
+              html: '<div style="' + (s.ruletype as text).cssstyle + '">' + value + '</div>',
+            }),
+            zIndexOffset: 1000, // Make appear above other map features
+          });
+          //apply the offset
+
+          label.addTo(geo);
+        }
+        break;
+      }
+    }
+    return geo;
   }
 }
