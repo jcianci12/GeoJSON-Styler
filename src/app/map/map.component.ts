bc14bbe81b5ebee9711e94f969c9c4d297dc64ef
CianciJ <CianciJ@QH11329889>
2023-07-06 14:27:41 +1000

    test push

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -269,6 +269,7 @@ export class MapComponent implements OnInit {
           }
           PathOptions.stroke = true;
           PathOptions.fill = true;
+
           break;
         }
         case 'text': {
